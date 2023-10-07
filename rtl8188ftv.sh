fc4b682deb8c7d49ba862ca28cd6f13ce4303506
Faizal <74812515+ioresrtl@users.noreply.github.com>
2023-10-08 00:28:20 +0600

    Update rtl8188ftv.sh

--- rtl8188ftv.sh
+++ rtl8188ftv.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 yes | (sudo apt update;sudo apt install net-tools
-//need to somehow have this ppa thing installed first
 sudo add-apt-repository ppa:kelebek333/kablosuz
 sudo apt-get update
 sudo apt-get install rtl8188fu-dkms
 echo “options rtl8188fu rtw_ips_mode=0” | sudo tee /etc/modprobe.d/rtl8188fu.conf
-sudo modprobe -rv rtl8188fu && sudo modprobe -v rtl8188fu) 
-
-
-
-
-
+sudo modprobe -rv rtl8188fu && sudo modprobe -v rtl8188fu)
